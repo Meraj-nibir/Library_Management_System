55fe7d3532ad640ff34f48733e3feb7abd7b2907
Meraj <merajurrahmanniber@gmail.com>
2023-10-16 01:13:23 +0600

    LMS class developing

---
+++ LMS.py
@@ -0,0 +1,11 @@
+import datetime
+import os
+#os.getcwd()
+
+
+class LMS:
+    #this class for keep record pf books
+    def __init__(self, list_of_books, library_name):
+        self.list_of_books = "List_of_books.txt"
+        self.library_name
+        
